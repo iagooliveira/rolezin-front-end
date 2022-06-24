d2814e7efbb45c2e86fcda8f39ff4d7476537fdb
Caio Oliveira Nascimento <caio.onascimento@hotmail.com>
2022-06-24 20:04:16 -0300

    menu fresco

--- src/modules/App/index.jsx
+++ src/modules/App/index.jsx
@@ -1,7 +1,19 @@
 import MainPage from "../MainPage/index.jsx";
-
+import MainHeader from "../MainPage/submodules/MainHeader";
+import Footer from "../MainPage/submodules/footer/footer.jsx";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 const App = () => {
-  return <MainPage />;
+  return ( 
+  <>
+  <Router>
+    <MainHeader />
+    <Switch>
+      <Route path='/' exact component={MainPage} />
+    </Switch>
+    <Footer/>
+  </Router>
+  </>
+  );
 };
 
 export default App;
--- src/modules/MainPage/index.jsx
+++ src/modules/MainPage/index.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import FirstSection from "./submodules/HomeSection";
 import SecondSection from "./submodules/secondSection/secondSection";
 import UltimaSecao from "./submodules/thirdSection/thirdSection";
-import MainHeader from "./submodules/MainHeader";
-import Footer from "./submodules/footer/footer.jsx";
+
 
 const MainPage = () => {
   return (
     <>
-      <MainHeader />
+      
       <FirstSection />
       <SecondSection/>
       <UltimaSecao/>
-      <Footer/>
+      
     </>
   );
 };
